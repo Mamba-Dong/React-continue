fd7304e33e471dee5c712f9f48cd108732c9a76e
donghongda <1300859154@qq.com>
2021-01-26 17:52:01 +0800

    添加登出功能

--- src/page/Header.js
+++ src/page/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Popover, Button } from "antd";
+import { useHistory } from "react-router-dom";
 import './Header.scss'
 
+import { UserOutlined } from '@ant-design/icons';
+
 function Header() {
-    return (
-        <div className="header">
-            {/*<p className="title">我还没想好要怎么做，就让它先在这滚吧。。</p>*/}
+    const history = useHistory()
+
+    const content = (
+        <div className="user">
+            <div>用户： admin</div>
+            <Button type="primary" danger className="logout" onClick={handleLogoutClick}>退出</Button>
         </div>
     )
+
+    return (
+        <div className="header">
+            <Popover content={content} trigger="click" placement="topRight" arrowPointAtCenter>
+                <UserOutlined />
+            </Popover>
+        </div>
+    )
+
+    function handleLogoutClick() {
+        history.push({ pathname: '/login' })
+    }
 }
 
 export default Header;
